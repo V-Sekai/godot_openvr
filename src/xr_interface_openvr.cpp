881e8b95f67af7a2f6fa53fdb2b2f3d42631856c
K. S. Ernest (iFire) Lee <ernest.lee@chibifire.com>
2021-10-24 14:13:52 -0700

    Moving order.

--- src/xr_interface_openvr.cpp
+++ src/xr_interface_openvr.cpp
@@ -403,8 +403,6 @@ void XRInterfaceOpenVR::_commit_views(const RID &p_render_target, const Rect2 &p
 
 		vr::Texture_t texture_right = { &vulkan_data_right, vr::TextureType_Vulkan, vr::ColorSpace_Gamma };
 		vr::VRCompositor()->Submit(vr::Eye_Right, &texture_right, &bounds, vr::Submit_VulkanTextureWithArrayData);
-		ovr->wait_get_poses();
-		ovr->update_poses();
 	}
 }
 
@@ -416,6 +414,8 @@ void XRInterfaceOpenVR::_commit_views(const RID &p_render_target, const Rect2 &p
 void XRInterfaceOpenVR::_process() {
 	if (ovr != nullptr && ovr->is_initialised()) {
 		vr::VRCompositor()->PostPresentHandoff();
+		ovr->wait_get_poses();
+		ovr->update_poses();
 		// Call process on our ovr system.
 		ovr->process();
 	}
