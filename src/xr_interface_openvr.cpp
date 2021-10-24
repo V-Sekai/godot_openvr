fcbfc266a69c864297adcc2302a6aaccc9ce0102
K. S. Ernest (iFire) Lee <ernest.lee@chibifire.com>
2021-10-24 12:23:27 -0700

    Try again.

--- src/xr_interface_openvr.cpp
+++ src/xr_interface_openvr.cpp
@@ -264,9 +264,8 @@ Vector2 XRInterfaceOpenVR::_get_render_target_size() {
 
 ////////////////////////////////////////////////////////////////
 // Informs Godot how many views are required
-int64_t XRInterfaceOpenVR::_get_view_count() {		
-	ovr->wait_get_poses();
-	ovr->update_poses();
+int64_t XRInterfaceOpenVR::_get_view_count() {
+	vr::VRCompositor()->SubmitExplicitTimingData();
 	return 2;
 }
 
@@ -404,7 +403,6 @@ void XRInterfaceOpenVR::_commit_views(const RID &p_render_target, const Rect2 &p
 
 		vr::Texture_t texture_right = { &vulkan_data_right, vr::TextureType_Vulkan, vr::ColorSpace_Gamma };
 		vr::VRCompositor()->Submit(vr::Eye_Right, &texture_right, &bounds, vr::Submit_VulkanTextureWithArrayData);
-		vr::VRCompositor()->SubmitExplicitTimingData();
 	}
 }
 
@@ -415,6 +413,9 @@ void XRInterfaceOpenVR::_commit_views(const RID &p_render_target, const Rect2 &p
 // next frame.
 void XRInterfaceOpenVR::_process() {
 	if (ovr != nullptr && ovr->is_initialised()) {
+		vr::VRCompositor()->PostPresentHandoff();
+		ovr->wait_get_poses();
+		ovr->update_poses();
 		// Call process on our ovr system.
 		ovr->process();
 	}
