81af4716357bf2ea22f0f82c7082850e99b7cc7b
K. S. Ernest (iFire) Lee <ernest.lee@chibifire.com>
2021-10-24 15:04:32 -0700

    Revert "Submit timing data earlier."
    This reverts commit edddc0edfc0ec369e6c34244999ac4a8f1ea4cb4.

--- src/xr_interface_openvr.cpp
+++ src/xr_interface_openvr.cpp
@@ -265,7 +265,6 @@ Vector2 XRInterfaceOpenVR::_get_render_target_size() {
 ////////////////////////////////////////////////////////////////
 // Informs Godot how many views are required
 int64_t XRInterfaceOpenVR::_get_view_count() {
-	vr::VRCompositor()->SubmitExplicitTimingData();
 	return 2;
 }
 
@@ -403,6 +402,7 @@ void XRInterfaceOpenVR::_commit_views(const RID &p_render_target, const Rect2 &p
 
 		vr::Texture_t texture_right = { &vulkan_data_right, vr::TextureType_Vulkan, vr::ColorSpace_Gamma };
 		vr::VRCompositor()->Submit(vr::Eye_Right, &texture_right, &bounds, vr::Submit_VulkanTextureWithArrayData);
+		vr::VRCompositor()->SubmitExplicitTimingData();
 	}
 }
 
